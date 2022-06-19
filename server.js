898bea676103a9e038a2154e3fa6b6819176c43a
Edwin0204 <edwins0204@gmail.com>
2022-06-19 12:50:17 -0500

    Server Routes and middleware

--- server.js
+++ server.js
@@ -3,3 +3,16 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/apiRoutes');
+
+//Middleware for urlencoded data
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+// Routes
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+app.listen(PORT, () => {
+    console.log(`API server online in http://localhost:${PORT}.`);
+  });
